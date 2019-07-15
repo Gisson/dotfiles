23b1d1ed1a12207ac6c8778c646f040168114ce4
Jorge Heleno <jorge.heleno@tecnico.ulisboa.pt>
2019-07-16 00:06:07 +0100

    Add checks for battery

--- .config/awesome/batteryhelpers.lua
+++ .config/awesome/batteryhelpers.lua
@@ -1,3 +1,4 @@
+local battery_helper = {}
 
 local function getMaxBattery()
 	local f = io.open(battery.path1 ..  "charge_full" , "rb")
@@ -17,3 +18,17 @@ end
 local function getBatteryPercent()
 	return (getCurrentBattery()*100) / getMaxBattery()
 end
+
+function battery_helper.is_battery ()
+	local battery = require("battery")
+	local f = io.open(battery.path1)
+	if f~=nil
+	then
+		io.close(f)
+		return true
+	else
+		return false
+	end
+end
+
+return battery_helper
